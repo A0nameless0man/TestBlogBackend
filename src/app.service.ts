38bc23b6d1bffc595e3652b2dd1bba8e6e63036e
huguang.nemo <huguang.nemo@bytedance.com>
2022-01-01 12:08:19 +0800

    root to router

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { Request as ExpressRequest, Router } from "express";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(req: ExpressRequest) {
+    const router = req.app._router as Router;
+    return {
+      routes: router.stack
+        .map(layer => {
+          if (layer.route) {
+            const path = layer.route?.path;
+            const method = layer.route?.stack[0].method;
+            return `${method.toUpperCase()} ${path}`
+          }
+        })
+        .filter(item => item !== undefined)
+    }
   }
 }
