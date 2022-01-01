df3802e4413216fdd17952d29cfd503fba41417c
huguang.nemo <huguang.nemo@bytedance.com>
2022-01-02 01:21:03 +0800

    $(git status)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import User from './user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({ type: 'sqlite', database: 'data.sqlite' })],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'data.sqlite',
+      autoLoadEntities: true,
+      synchronize: true // auto create tables do not do this in prod env
+    }),
+    TypeOrmModule.forFeature([User])],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
